07276ca1a1a8cf0abab5ebe57b5c7b1d54c1e88f
MAGICAL-KX <xkenton0@gmail.com>
2024-09-07 10:03:55 +0200

    Add files via upload

---
+++ commands/developers.js
@@ -0,0 +1,101 @@
+const {fetchJson,cmd, tlang,sleep } = require('../lib')
+
+//---------------------------------------------------------------------------
+
+cmd({
+
+            pattern: "Developer",
+
+            category: "tools",
+
+            desc: "Developers info",
+
+            use: ' ',
+
+            filename: __filename,
+
+        },
+
+        async(Void,citel, text) => {
+
+citel.reply("```Sending developer information```")
+
+await sleep(1000)
+
+citel.reply("```🅢𝚊𝚕𝚖𝚊𝚗 🅐𝚑𝚖𝚊𝚍:-𝙼𝙰𝙸𝙽 𝙳𝙴𝚅```")
+
+
+await sleep(1000)
+
+citel.reply("```🅔𝚡𝚌𝚎𝚕 🅐𝚖𝚊𝚍𝚒\n https://Xcelsama```")
+
+await sleep(1000)
+
+citel.reply("```🅐𝚋𝚛𝚊𝚑𝚊𝚖 🅓𝚠𝚊𝚗𝚖𝚎𝚗𝚊 Qr ideas```")
+
+await sleep(1000)
+
+citel.reply("```🅢uhail🅢er \n Bug fixies```")
+
+await sleep(1000)
+
+citel.reply("```🅢am 🅟adey\n Base```")
+
+await sleep(1000)
+
+citel.reply("```OTHER BOTS FROM DEVELOPER```")
+
+await sleep(1000)
+
+citel.reply("```ᎬХᏟᎬᏞ:-\n https://github.com/Xcelsama/STAR-MD-V2```")
+
+await sleep(1000)
+
+citel.reply("```ՏᎪᏞᎷᎪΝ:- https://github.com/salmanytofficial/XLICON-V3-MD```")
+
+await sleep(1000)
+
+citel.reply("```SAM:-\n https://github.com/SamPandey001/Secktor-MD```")
+
+await sleep(1000)
+
+citel.reply("```ᴱ𝗑Ꮯ𝖊ℓ:-\n https://github.com/Xcelsama/STAR-MD```")
+
+await sleep(1000)
+
+citel.reply("```🅢𝚞𝚑𝚊𝚒𝚕 🅢𝚎𝚛\𝚗 https://github.com/SuhailTechInfo/Suhail-Md```")
+
+await sleep(1000)
+
+citel.reply("```EX-BOTS:- https://EX-BOTS/BAT-MD")
+
+await sleep(1000)
+
+citel.reply("```https://github.com/salmanytofficial/XLICON-V2-MD```")
+
+await sleep(1000)
+
+citel.reply("```https://github.com/V-E-N-O-X/IRIS-MD```")
+
+await sleep(1000)
+
+citel.reply("``` https://github.com/AbhishekSuresh2/Phoenix-MD ```")
+
+await sleep(1000)
+
+citel.reply("```https://github.com/Maher-Zubair/SIGMA ```")
+
+await sleep(1000)
+
+citel.reply("```𝑇𝐸𝐴𝑀-𝑋𝐿𝐼𝐶𝑂𝑁 ```")
+
+await sleep(1000) 
+
+citel.reply("```ʙʏ ᴇxᴄᴇʟ 2347045035241```")
+
+
+            return citel.reply('*THOSE ARE MY DEVELOPERS *');
+
+        }
+
+    )
---
+++ commands/hack.js
@@ -0,0 +1,101 @@
+const {fetchJson,cmd, tlang,sleep } = require('../lib')
+
+//---------------------------------------------------------------------------
+
+cmd({
+
+            pattern: "hack",
+
+            category: "fun",
+
+            desc: "hacking prank",
+
+            use: ' ',
+
+            filename: __filename,
+
+        },
+
+        async(Void,citel, text) => {
+
+citel.reply("```Initializing hacking sequence```")
+
+await sleep(1000)
+
+citel.reply("``` Scanning Target🎯 Network📡for Vulnerabilities```")
+
+
+await sleep(1000)
+
+citel.reply("```By-passing firewall  intrusion  detection🔎 system```")
+
+await sleep(1000)
+
+citel.reply("```Gaining Unauthorized access to target device\n  █ 10%```")
+
+await sleep(1000)
+
+citel.reply("```Downloading Sensitive data \n  █ █ 20%```")
+
+await sleep(1000)
+
+citel.reply("```Analyzing target 🎯 personal files\n █ █ █ █ 40%```")
+
+await sleep(1000)
+
+citel.reply("```Encrypting data for transfer█ █ █ █ █ 50%```")
+
+await sleep(1000)
+
+citel.reply("```Establishing secure connection to remote server📡\n █ █ █ █ █ █ 60%```")
+
+await sleep(1000)
+
+citel.reply("```Transfering encrypted data to Excelsior server\n █ █ █ █ █ █ █ 70%```")
+
+await sleep(1000)
+
+citel.reply("```Transfer successful \n █ █ █ █ █ █ █ █ 80%```")
+
+await sleep(1000)
+
+citel.reply("```Transfering Browser History \n █ █ █ █ █ █ █ █ █ 90%```")
+
+await sleep(1000)
+
+citel.reply("```Transfer successful \n █ █ █ █ █ █ █ █ █ █ 100% ```")
+
+await sleep(1000)
+
+citel.reply("```System hyjacking on process.. \n Conecting to Excelsior Server ```")
+
+await sleep(1000)
+
+citel.reply("```Device successfully connected... \n Riciving data...```")
+
+await sleep(1000)
+
+citel.reply("```Data hyjacked from divice 100% completed \n killing all evidence killing all malwares...```")
+
+await sleep(1000)
+
+citel.reply("``` HACKING COMPLETED ```")
+
+await sleep(1000)
+
+citel.reply("``` SENDING  DOCUMENTS...```")
+
+await sleep(1000)
+
+citel.reply("``` SUCCESSFULLY SENT DATA AND Connection disconnected```")
+
+await sleep(1000) 
+
+citel.reply("```ʙʏ ᴇxᴄᴇʟ 2347045035241```")
+
+
+            return citel.reply('*ALL FILES TRANSFERRED*');
+
+        }
+
+    )
